d9ccfc5001612deadfbee33cdee5f856635997d1
nst-student <khuswantrajpurohit18@gmail.com>
2025-10-05 15:18:51 +0530

    server.js updated

--- Backend/server.js
+++ Backend/server.js
@@ -3,22 +3,22 @@ import dotenv from "dotenv"
 import commentRoutes from './routes/comment.route.js'
 import cors from "cors"
 
-const allowedOrigins = [
-  "http://localhost:5173", "http://localhost:5174",
-  "https://comment-system-self.vercel.app/", 
-];  
+// const allowedOrigins = [
+//   "http://localhost:5173", "http://localhost:5174",
+//   "https://comment-system-self.vercel.app/", 
+// ];  
  
 
 const app = express(); 
 app.use(express.json())
 app.use(cors());
-app.use(
-  cors({
-    origin: allowedOrigins,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-); 
+// app.use(
+//   cors({
+//     origin: allowedOrigins,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// ); 
 dotenv.config() 
 
 app.get("/", (req, res) => { 
